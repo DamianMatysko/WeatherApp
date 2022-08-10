722decc8cdf4e49fe14a269ed73dd00271f387d4
Damian Matysko <damian.matysko@gmail.com>
2022-08-10 14:34:39 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Damián Matysko on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
